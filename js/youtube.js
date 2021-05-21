57f7b1fe1541f293a48caab9f7afcc8577c9df08
JinanJeong <jeongjinan123@gmail.com>
2021-05-22 03:08:27 +0900

    'FisrtProject'

--- js/youtube.js
+++ js/youtube.js
@@ -14,6 +14,11 @@ function onYouTubeIframeAPIReady() {
       autoplay: true, // 자동 재생 유무
       loop: true, // 반복 재생 유무
       videoId: 'An6LvWQuj_8', // 반복 재생할 유튜브 영상 ID 목록
+    },
+    events: {
+      onReady: function (event) {
+        event.target.mute() //음소거
+      }
     }
   });
 }
